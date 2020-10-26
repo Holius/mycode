52f97c265a289b9c55ffaa21989e42feeb8acb69
holius <justindgilley@gmail.com>
2020-10-26 21:41:41 +0000

    work with lists

---
+++ concatTime.py
@@ -0,0 +1,4 @@
+#!bin/env python3
+name = input("Enter first name: ")
+day = input("Enter the day of the week: ")
+print("Hello, " + name + "! Happy " + day + "!")
---
+++ mixlist01.py
@@ -0,0 +1,6 @@
+l = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+new_str = f"When I {l[-1]} into IP addresses {l[3]} or {l[4]} I am unable to ping ports {l[0]}, {l[1]}, or {l[2]}."
+
+print(new_str)
+
